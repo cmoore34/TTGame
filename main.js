1a3cd7759476cd63b1f3c1e051ada6659b251e4b
James Moore <jmoo7036@na.convergys.com>
2019-06-11 13:45:45 -0400

    added some color so you can see the hand initially

--- main.js
+++ main.js
@@ -1,49 +1,45 @@
 class Deck {
     constructor() {
-        this.deck = ["Wheat","Wood","Brick","Stone","Glass","Wheat","Wood","Brick","Stone","Glass","Wheat","Wood","Brick","Stone","Glass"];
+        this.deck = ["Wheat", "Wood", "Brick", "Stone", "Glass", "Wheat", "Wood", "Brick", "Stone", "Glass", "Wheat", "Wood", "Brick", "Stone", "Glass"];
         this.hand = [];
         this.played = "";
         this.shuffle();
     }
-  
-    shuffle(){
-      const { deck } = this;
-      let m = deck.length, i;
-  
-      while(m){
-        i = Math.floor(Math.random() * m--);
-  
-        [deck[m], deck[i]] = [deck[i], deck[m]];
-      }
-  
-      return this;
-    }
-  
-    deal(){
-      return this.hand.push(this.deck.pop());
+
+    shuffle() {
+        const { deck } = this;
+        let m = deck.length,
+            i;
+
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+
+            [deck[m], deck[i]] = [deck[i], deck[m]];
+        }
+
+        return this;
     }
 
-    play(cardnum)
-    {
-        this.played = this.hand.splice(cardnum,1).toString()
+    deal() {
+        return this.hand.push(this.deck.pop());
+    }
+
+    play(cardnum) {
+        this.played = this.hand.splice(cardnum, 1).toString()
         this.deck.unshift(this.played);
     }
-  }
-  
-  const game = new Deck();
-  for(i=0;i<3;i++)
-  {
-    game.deal();
-  }
-  
-  console.log(game.deck);
-  console.log(game.hand);
-  refreshHand();
+}
 
-  function refreshHand(){
-    for(i=0; i < game.hand.length; i++)(function(){
-      $('.card'+i).addClass(game.hand[i]);
-    });
-  }
-  
-  
+const game = new Deck();
+for (i = 0; i < 3; i++) {
+    game.deal();
+}
+
+function refreshHand() {
+    for (i = 0; i < game.hand.length; i++) {
+        $('#card' + i).addClass(game.hand[i].toString().toLowerCase());
+    };
+}
+
+console.log(game.deck);
+console.log(game.hand);
